e12fab8ab80a4bc843e22b2e95629d8257e8b303
RedBerserk <nosbit@mail.ru>
2022-10-11 13:55:47 +0300

    docs: solve 1 task

--- src/count-cats.js
+++ src/count-cats.js
@@ -3,7 +3,7 @@ const { NotImplementedError } = require('../extensions/index.js');
 /**
  * Given matrix where you have to find cats by ears "^^"
  *
- * @param {Array<Array>} matrix 
+ * @param {Array<Array>} matrix
  * @return {Number} count of cats found
  *
  * @example
@@ -22,3 +22,16 @@ function countCats(/* matrix */) {
 module.exports = {
   countCats
 };
+
+function countCats(backyard){
+  let count = 0;
+  for (let i = 0; i < backyard.length; i++){
+    let arr = backyard[i];
+    for(let j = 0; j < arr.length; j++){
+      if(arr[j] == '^^'){
+        count++;
+      }
+    }
+  }
+  return count;
+}
